9b5f10b482d18ef685fed959c35c285cc0977b6b
Mahmood Ahmed <Mahmood.Ahmed.SE.@outlook.com>
2025-08-18 00:15:41 +0400

    fix: added logic for next.js api

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,1 +1,2 @@
 export const EXTERNAL_API_ADDR = "https://127.0.0.1:4040";
+export const MOVIE_POSTER_IMAGES_PATH = "/home/mahmood/proj/MovieRatingSoftware/movie-data-scraper"
--- src/app/api/movie-image/[movieId]/route.ts
+++ src/app/api/movie-image/[movieId]/route.ts
@@ -1,15 +1,40 @@
 import fs from 'fs';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
+import { EXTERNAL_API_ADDR, MOVIE_POSTER_IMAGES_PATH } from '../../config';
 
 
 
 export const GET = async (
-  req: NextRequest,
-  { params }: { params: Promise<{ movieId: string }> }
+    req: NextRequest,
+    { params }: { params: Promise<{ movieId: string }> }
 ) => {
     const { movieId } = await params;
-    
 
-    
+    const resp = await fetch(`${EXTERNAL_API_ADDR}/api/v1/image-name/${movieId}`);
+    if (resp.status != 200) {
+        return NextResponse.json({
+            "message": "error occured",
+        })
+    }
+
+    const data = await resp.json();
+
+
+    const filePath = path.join(MOVIE_POSTER_IMAGES_PATH, data.directory)
+
+    if (!fs.existsSync(filePath)) {
+        return NextResponse.json('Not found', {
+            status: 404,
+        });
+    }
+
+    const imageBuffer = fs.readFileSync(filePath);
+
+    return new NextResponse(imageBuffer, {
+        headers: {
+            'Content-Type': 'image/png',
+            'Content-Length': imageBuffer.length.toString(),
+        },
+    });
 }
