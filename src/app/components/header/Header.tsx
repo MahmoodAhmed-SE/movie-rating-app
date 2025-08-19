d1c9a5bf3ff5fdcaab1590901598b82ce5ec8360
Mahmood Ahmed <Mahmood.Ahmed.SE.@outlook.com>
2025-08-19 21:37:31 +0400

    feat: added sidebar

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -2,11 +2,47 @@ import Link from 'next/link';
 import styles from './header.module.css';
 import Logo from './logo';
 import ProfileButton from './profileButton';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const sidebarRef = useRef<HTMLDivElement>(null);
+    const menuButtonRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleMouseIsOutside = (e: MouseEvent) => {
+            if (
+                sidebarRef.current &&
+                !sidebarRef.current.contains(e.target as Node) &&
+                menuButtonRef.current &&
+                !menuButtonRef.current.contains(e.target as Node)
+            ) {
+                setIsMenuOpen(false)
+            }
+        }
+
+        document.addEventListener('mousedown', handleMouseIsOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleMouseIsOutside)
+        }
+    })
     return (
         <header className={styles.header}>
-            <img src="/header/menu_button.svg" alt="Menu button." />
+            <div
+                ref={menuButtonRef}
+                className={[styles.menu_button, isMenuOpen ? styles.open : ''].join(' ')}
+                onClick={() => setIsMenuOpen((prevVal) => !prevVal)}
+            >
+                <img
+                    src={isMenuOpen ? "/header/close_menu_button.svg" : "/header/menu_button.svg"}
+                    alt="menu toggle"
+                    className={styles.menu_icon}
+                />
+            </div>
+            <div className={[styles.sidebar_container, isMenuOpen ? styles.open : ''].join(' ')} ref={sidebarRef}>
+                
+            </div>
         </header>
     );
 }
