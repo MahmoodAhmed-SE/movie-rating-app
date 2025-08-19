b7dcdb39b0b83708107ac266457e99f063b4d9ba
Mahmood Ahmed <Mahmood.Ahmed.SE@outlook.com>
2025-08-19 14:35:52 +0400

    fix: added cookie to api image-path GET request

--- src/app/api/movie-image/[movieId]/route.ts
+++ src/app/api/movie-image/[movieId]/route.ts
@@ -1,40 +1,46 @@
-import fs from 'fs';
-import { NextRequest, NextResponse } from 'next/server';
-import path from 'path';
-import { EXTERNAL_API_ADDR, MOVIE_POSTER_IMAGES_PATH } from '../../config';
-
-
+import fs from "fs";
+import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import { EXTERNAL_API_ADDR, MOVIE_POSTER_IMAGES_PATH } from "../../config";
 
 export const GET = async (
-    req: NextRequest,
-    { params }: { params: Promise<{ movieId: string }> }
+  req: NextRequest,
+  { params }: { params: Promise<{ movieId: string }> }
 ) => {
-    const { movieId } = await params;
+  const { movieId } = await params;
 
-    const resp = await fetch(`${EXTERNAL_API_ADDR}/api/v1/image-name/${movieId}`);
-    if (resp.status != 200) {
-        return NextResponse.json({
-            "message": "error occured",
-        })
+  const cookieHeader = req.headers.get("cookie");
+  const resp = await fetch(
+    `${EXTERNAL_API_ADDR}/api/v1/image-path/${movieId}`,
+    {
+      method: "GET",
+      headers: {
+        ...(cookieHeader ? { cookie: cookieHeader } : {}),
+      },
     }
-
-    const data = await resp.json();
-
-
-    const filePath = path.join(MOVIE_POSTER_IMAGES_PATH, data.directory)
-
-    if (!fs.existsSync(filePath)) {
-        return NextResponse.json('Not found', {
-            status: 404,
-        });
-    }
-
-    const imageBuffer = fs.readFileSync(filePath);
-
-    return new NextResponse(imageBuffer, {
-        headers: {
-            'Content-Type': 'image/png',
-            'Content-Length': imageBuffer.length.toString(),
-        },
+  );
+  if (resp.status != 200) {
+    return NextResponse.json({
+      message: "error occured",
     });
-}
+  }
+
+  const data = await resp.json();
+
+  const filePath = path.join(MOVIE_POSTER_IMAGES_PATH, data.directory);
+
+  if (!fs.existsSync(filePath)) {
+    return NextResponse.json("Not found", {
+      status: 404,
+    });
+  }
+
+  const imageBuffer = fs.readFileSync(filePath);
+
+  return new NextResponse(imageBuffer, {
+    headers: {
+      "Content-Type": "image/png",
+      "Content-Length": imageBuffer.length.toString(),
+    },
+  });
+};
